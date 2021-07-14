662cec18433b1ad56f4cc6ea13af704da81fff15
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-07-14 20:26:02 +0500

    Use a Map in the TaskCollection.ts

--- todo/src/TaskCollection.ts
+++ todo/src/TaskCollection.ts
@@ -2,11 +2,16 @@ import { Task } from "./Task";
 
 export class TaskCollection {
   private nextId = 1;
+  private itemMap = new Map<number, Task>();
   constructor(public tasks: Task[] = []) {}
   public addTodo(task: string): void {
     let item: Task = new Task(this.nextId, task, false);
     this.nextId++;
     this.tasks.push(item);
+    this.itemMap.set(this.nextId, new Task(this.nextId, task));
+  }
+  public getTodoById(id: number): Task {
+    return this.itemMap.get(id);
   }
   public taskDone(taskId: number): void {
     let task: Task = this.tasks.find((task) => task.taskId == taskId);
